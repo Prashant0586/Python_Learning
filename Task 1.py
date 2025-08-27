32b522e933202a8d527ffedd830760f1e338320d
Prashant0586 <99256025+Prashant0586@users.noreply.github.com>
2025-08-27 15:13:01 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,6 @@
+# Checks whether the number is even or odd using an if-else statement.
+a=int(input("Enter a Number::"))
+if a%2==0:
+    print(a, "is an Even Number")
+else:
+    print(a, "is an Odd Number")
---
+++ Task 2.py
@@ -0,0 +1,5 @@
+sum=0
+i=1
+for i in range(1,51):
+    sum+=i
+print("The sum of Numbers from 1 to 50 is ::",sum)
